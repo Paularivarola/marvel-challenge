1e66c3f1eeeac4bc99349dc377569d7953b782cb
Paularivarola <paula.rivarola02@gmail.com>
2025-08-28 05:48:09 +0200

    build(api): use VITE_API_BASE for axios baseURL

--- src/services/axios-config.ts
+++ src/services/axios-config.ts
@@ -1,9 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
+
+const VITE_API_BASE = (import.meta.env.VITE_API_BASE ?? "").replace(/\/$/, "");
+
+const BASE_URL = VITE_API_BASE ? `${VITE_API_BASE}/api` : "/api";
 
 export const AxiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: BASE_URL,
   timeout: 10000,
-})
+  headers: { Accept: "application/json" },
+});
 
 AxiosInstance.interceptors.response.use(
   (r) => r,
